fe2c3195bd7b02fffd67efa8811fe342ab7df04c
sandesh <sandesh@trisysit.com>
2018-06-30 23:18:27 +0530

    fixed shifing mode of items

--- app/src/main/java/com/example/youtubelikebackstack/MainActivity.java
+++ app/src/main/java/com/example/youtubelikebackstack/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.youtubelikebackstack;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +25,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    addFragment(getSupportFragmentManager(),new HomeFragment(),R.id.frame,true);
+                    addFragment(getSupportFragmentManager(), new HomeFragment(), R.id.frame, true);
                     return true;
                 case R.id.navigation_dashboard:
-                    addFragment(getSupportFragmentManager(),new DashboardFragment(),R.id.frame,false);
+                    addFragment(getSupportFragmentManager(), new DashboardFragment(), R.id.frame, false);
                     return true;
                 case R.id.navigation_notifications:
-                    addFragment(getSupportFragmentManager(),new NotificationFragment(),R.id.frame,false);
+                    addFragment(getSupportFragmentManager(), new NotificationFragment(), R.id.frame, false);
                     return true;
                 case R.id.navigation_setting:
-                    addFragment(getSupportFragmentManager(),new SettingFragment(),R.id.frame,false);
+                    addFragment(getSupportFragmentManager(), new SettingFragment(), R.id.frame, false);
                     return true;
             }
             return false;
@@ -42,34 +47,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = findViewById(R.id.navigation);
+        disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_home);
     }
 
     public void addFragment(FragmentManager fragmentManager,
-                                   Fragment fragment,
-                                   int containerId,boolean isFromHome){
+                            Fragment fragment,
+                            int containerId, boolean isFromHome) {
 
-        fragmentManager.popBackStack(null,FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
 
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        if(isFromHome){
-            fragmentTransaction.replace(containerId,fragment);
-        }else{
-            fragmentTransaction.add(new HomeFragment(),"Home");
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isFromHome) {
+            fragmentTransaction.replace(containerId, fragment);
+        } else {
+            fragmentTransaction.add(new HomeFragment(), "Home");
             fragmentTransaction.addToBackStack("Home");
         }
-        fragmentTransaction.replace(containerId,fragment).commit();
+        fragmentTransaction.replace(containerId, fragment).commit();
 
     }
 
     @Override
     public void onBackPressed() {
-        if(getSupportFragmentManager().getBackStackEntryCount()>0){
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
             navigation.setSelectedItemId(R.id.navigation_home);
-        }else {
+        } else {
             super.onBackPressed();
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
         }
     }
 }
